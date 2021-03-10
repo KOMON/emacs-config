c5c90e58297da8d09fa3246da0d33215bb512510
Daniel Raloff <dan.raloff@koan.co>
2021-03-10 15:25:48 -0800

    Add ansi coloring to magit buffers

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
   (add-to-list 'custom-theme-load-path "~/.emacs.d/elpa"))
 
 (use-package moe-theme
+  :ensure t
   :init
   (load-theme 'moe-dark t)
   :config
@@ -36,21 +37,68 @@
    ("C-c a" . 'org-agenda)))
 
 (use-package auto-virtualenvwrapper
+  :ensure t
   :config
   (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
   (add-hook 'projectile-after-switch-project-hook #'auto-virtualenvwrapper-activate))
 
-(use-package ws-butler)
+(use-package ws-butler
+  :ensure t)
 
-(use-package terraform-mode)
+(use-package terraform-mode
+  :ensure t)
 
 (use-package flycheck
+  :ensure t
   :init (global-flycheck-mode)
   :config
   (setq-default flycheck-disabled-checkers
                 (append flycheck-disabled-checkers
                         '(javascript-jshint)))
-  (flycheck-add-mode 'javascript-eslint 'web-mode))
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (flycheck-define-checker koan-lint
+    "doc string"
+    :command ("yarn" "lint" "--format=json" "--stdin" "--stdin-filename" source-original)
+    :standard-input t
+    :error-parser flycheck-parse-eslint
+    :enabled (lambda () (flycheck-eslint-config-exists-p))
+    :modes (js-mode js-jsx-mode js2-mode js2-jsx-mode js3-mode rjsx-mode
+                    typescript-mode)
+    :working-directory flycheck-eslint--find-working-directory
+    :verify
+    (lambda (_)
+      (let* ((default-directory
+               (flycheck-compute-working-directory 'javascript-eslint))
+             (have-config (flycheck-eslint-config-exists-p)))
+        (list
+         (flycheck-verification-result-new
+          :label "config file"
+          :message (if have-config "found" "missing or incorrect")
+          :face (if have-config 'success '(bold error))))))
+    :error-explainer
+    (lambda (err)
+      (let ((error-code (flycheck-error-id err))
+            (url "https://eslint.org/docs/rules/%s"))
+        (and error-code
+             ;; skip non-builtin rules
+             (not ;; `seq-contains-p' is only in seq >= 2.21
+              (with-no-warnings (seq-contains error-code ?/)))
+             `(url . ,(format url error-code))))))
+
+  (flycheck-define-checker koan-test
+    "doc string"
+    :command ("yarn" "test"
+              "--json"
+              "--testPathPattern" (eval buffer-file-name)
+              "--outputFile" (eval (flycheck-jest--result-path)))
+    :error-parser flycheck-jest--parse
+    :modes (web-mode js-mode typescript-mode rjsx-mode)
+    :predicate (lambda () (funcall #'flycheck-jest--should-use-p))
+    :working-directory
+    (lambda (checker)
+      (flycheck-jest--find-jest-project-directory checker)))
+  (add-to-list 'flycheck-checkers 'koan-lint)
+  (add-to-list 'flycheck-checkers 'koan-test))
 
 (use-package flycheck-jest
   :ensure t
@@ -85,6 +133,10 @@
       ("xr" "Restart" tide-restart-server)]])
   :config
   (add-hook 'typescript-mode-hook 'tide-custom)
+  (add-hook 'web-mode-hook 'tide-custom)
+  (add-hook 'js2-mode-hook 'tide-custom)
+  (flycheck-add-next-checker 'typescript-tide '(warning . koan-test))
+  (flycheck-add-next-checker 'typescript-tide '(error . koan-lint))
   :bind (:map tide-mode-map
               ("M-?" . 'tide-references)
               ("M-'" . 'tide-documentation-at-point)
@@ -93,11 +145,13 @@
 (use-package indium)
 
 (use-package yasnippet
+  :ensure t
   :config
   :init
   (yas-global-mode 1))
 
-(use-package string-inflection)
+(use-package string-inflection
+  :ensure t)
 
 (use-package syntax-subword
   :ensure t
@@ -111,21 +165,25 @@
   :bind (("C-x g" . magit-status)
          ("C-x M-g" . magit-dispatch-popup))
   :config
-  (global-magit-file-mode 't)
   (add-hook 'after-save-hook 'magit-after-save-refresh-status))
 
-;; (use-package magit-todos
-;;   :after magit
-;;   :config
-;;   (magit-todos-mode 1))
+(use-package magit-todos
+  :after magit
+  :config
+  (magit-todos-mode -1))
+
+(use-package forge
+  :after magit)
 
 (use-package projectile
   :ensure t
   :config
-  (setq projectile-completion-system 'helm
+   (setq projectile-completion-system 'helm
         projectile-switch-project-action #'projectile-commander)
 
   :bind-keymap ("C-c p" . projectile-command-map))
+
+(use-package flycheck-projectile)
 
 (use-package geiser
   :ensure t)
@@ -139,7 +197,7 @@
   :ensure t
   :init
   (add-to-list 'auto-mode-alist '("\\.phtml$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[tj]sx$" . web-mode))
   (defun my-web-mode-hook ()
     "Hooks for Web mode."
     (setq web-mode-markup-indent-offset 2)
@@ -172,6 +230,10 @@
 ;; (use-package company-phpactor
 ;;   :config
 ;;   (add-to-list 'company-backends 'company-phpactor))
+
+(use-package prettier
+  :config
+  (global-prettier-mode))
 
 (use-package php-mode
   :ensure t
@@ -230,6 +292,7 @@
   (add-hook 'php-mode-hook 'ws-butler-mode))
 
 (use-package company
+  :ensure t
   :bind
   (("M-/" . company-complete))
   :config
@@ -237,6 +300,7 @@
 
 
 (use-package go-mode
+  :ensure t
   :init
   (defun go-custom ()
     "go-mode-hook"
@@ -244,12 +308,6 @@
   :config
   (add-hook 'go-mode-hook 'go-custom))
 
-(use-package dumb-jump
-  :ensure t
-  :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go))
-  :config
-  (setq dumb-jump-selector 'helm))
 
 (use-package expand-region
   :ensure t
@@ -284,6 +342,7 @@
   (helm-mode 1))
 
 (use-package helm-swoop
+  :ensure t
   :config
   :bind (("M-i" . helm-swoop)
          ("M-I" . helm-multi-swoop)
@@ -294,12 +353,60 @@
          ("C-s" . helm-swoop-from-isearch)))
 
 (use-package helm-projectile
+  :ensure t
   :bind (:map projectile-command-map
               ("SPC" . helm-projectile))
   :config
   (helm-projectile-on))
 
-(use-package smex)
+;; (use-package selectrum
+;;   :config
+;;   (selectrum-mode +1)
+;;   (selectrum-prescient-mode +1)
+;;   (prescient-persist-mode +1))
+
+;; (use-package consult
+;;   :bind (("C-c h" . consult-history)
+;;          ("C-c o" . consult-outline)
+;;          ("C-x b" . consult-buffer)
+;;          ("C-x 4 b" . consult-buffer-other-window)
+;;          ("C-x 5 b" . consult-buffer-other-frame)
+;;          ("C-x r x" . consult-register)
+;;          ("C-x r b" . consult-bookmark)
+;;          ("M-s o" . consult-outline)
+;;          ("M-s m" . consult-mark)
+;;          ("M-s l" . consult-line)
+;;          ("M-s M-l" . consult-line)
+;;          ("M-s i" . consult-imenu)
+;;          ("M-s e" . consult-error)
+;;          ("M-s m" . consult-multi-occur)
+;;          ("M-y" . consult-yank-pop)
+;;          ("C-c ! !" . consult-flycheck)
+;;          ("<help> a" . consult-apropos))
+;;   :init
+;;   (fset 'multi-occur #'consult-multi-occur)
+
+;;   :config
+;;   (consult-preview-mode))
+
+;; (use-package marginalia
+;;   :init
+;;   (marginalia-mode)
+;;   (setq marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light))
+;;   (setq marginalia-command-categories
+;;         '((imenu . imenu)
+;;           (projectile-find-file . file)
+;;           (projectile-find-file-other-window . file)
+;;           (projectile-find-dir . file)
+;;           (projectile-find-test-file . file)
+;;           (projectile-recentf . file)
+;;           (projectile-find-dir-other-window . file)
+;;           (projectile-switch-to-buffer . buffer)
+;;           (projectile-switch-to-buffer-other-window . buffer)
+;;           (projectile-display-buffer . buffer))))
+
+(use-package smex
+  :ensure t)
 
 (use-package beacon
   :ensure t
@@ -307,7 +414,7 @@
   (beacon-mode 1)
   (setq beacon-color "white"))
 
-
+ 
 (use-package recentf
   :ensure t
   :config
@@ -393,12 +500,16 @@ Also, if the last command was a copy - skip past all the expand-region cruft."
 (require 'ansi-color)
 (defun djr/colorize-compilation-buffer ()
   "Colorize compilation buffer with ansi colors."
-  (toggle-read-only)
-  (ansi-color-apply-on-region compilation-filter-start (point))
-  (toggle-read-only))
+  (ansi-color-apply-on-region compilation-filter-start (point)))
 
 (add-hook 'compilation-filter-hook 'djr/colorize-compilation-buffer)
 
+(advice-add 'magit-process-filter
+            :after (lambda (proc &rest args)
+                     (with-current-buffer (process-buffer proc)
+                       (read-only-mode -1)
+                       (ansi-color-apply-on-region (point-min) (point-max))
+                       (read-only-mode 1))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -408,22 +519,19 @@ Also, if the last command was a copy - skip past all the expand-region cruft."
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(company-backends
-   (quote
-    (company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+   '(company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
                    (company-dabbrev-code company-etags company-keywords)
-                   company-oddmuse company-dabbrev)))
- '(custom-enabled-themes (quote (moe-dark)))
+                   company-oddmuse company-dabbrev))
+ '(custom-enabled-themes '(moe-dark))
  '(custom-safe-themes
-   (quote
-    ("7675ffd2f5cb01a7aab53bcdd702fa019b56c764900f2eea0f74ccfc8e854386" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" "ed0b4fc082715fc1d6a547650752cd8ec76c400ef72eb159543db1770a27caa7" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "4ee4a855548a7a966fe8722401441499b0d8b2fcf3d12438f81e016b6efed0e6" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "d411730c6ed8440b4a2b92948d997c4b71332acf9bb13b31e9445da16445fe43" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "17cda1304ba8d26d62bf247cab2c161d12957054b6be4477abb5972a74eea4e1" "715fdcd387af7e963abca6765bd7c2b37e76154e65401cd8d86104f22dd88404" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" default)))
+   '("7675ffd2f5cb01a7aab53bcdd702fa019b56c764900f2eea0f74ccfc8e854386" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" "ed0b4fc082715fc1d6a547650752cd8ec76c400ef72eb159543db1770a27caa7" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "4ee4a855548a7a966fe8722401441499b0d8b2fcf3d12438f81e016b6efed0e6" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "d411730c6ed8440b4a2b92948d997c4b71332acf9bb13b31e9445da16445fe43" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "17cda1304ba8d26d62bf247cab2c161d12957054b6be4477abb5972a74eea4e1" "715fdcd387af7e963abca6765bd7c2b37e76154e65401cd8d86104f22dd88404" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" default))
  '(debug-on-error nil)
- '(diary-entry-marker (quote font-lock-variable-name-face))
+ '(diary-entry-marker 'font-lock-variable-name-face)
  '(dired-bind-jump nil)
  '(display-line-numbers-type t)
- '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-split-window-function 'split-window-horizontally)
  '(emms-mode-line-icon-image-cache
-   (quote
-    (image :type xpm :ascent center :data "/* XPM */
+   '(image :type xpm :ascent center :data "/* XPM */
 static char *note[] = {
 /* width height num_colors chars_per_pixel */
 \"    10   11        2            1\",
@@ -441,25 +549,23 @@ static char *note[] = {
 \"#..######.\",
 \"#######...\",
 \"######....\",
-\"#######..#\" };")))
+\"#######..#\" };"))
  '(fci-rule-color "#222222")
  '(flycheck-php-phpcs-executable "/usr/local/bin/phpcs")
  '(flycheck-php-phpmd-executable "/usr/local/bin/phpmd")
  '(flycheck-phpcs-standard "PSR2")
  '(geben-dbgp-default-port 9001)
- '(geben-path-mappings
-   (quote
-    (("/Users/danr/src/magento-commerce-tfaw/tfaw_23" "/app"))))
+ '(geben-path-mappings '(("/Users/danr/src/magento-commerce-tfaw/tfaw_23" "/app")))
  '(geben-pause-at-entry-line nil)
  '(geben-predefined-breakpoints nil)
  '(geben-query-on-clear-breakpoints nil)
- '(ggtags-auto-jump-to-match (quote history))
+ '(ggtags-auto-jump-to-match 'history)
  '(global-aggressive-indent-mode nil)
  '(global-flycheck-mode t)
- '(gnus-logo-colors (quote ("#528d8d" "#c0c0c0")) t)
+ '(global-prettier-mode t)
+ '(gnus-logo-colors '("#528d8d" "#c0c0c0") t)
  '(gnus-mode-line-image-cache
-   (quote
-    (image :type xpm :ascent center :data "/* XPM */
+   '(image :type xpm :ascent center :data "/* XPM */
 static char *gnus-pointer[] = {
 /* width height num_colors chars_per_pixel */
 \"    18    13        2            1\",
@@ -479,33 +585,28 @@ static char *gnus-pointer[] = {
 \"######..###.######\",
 \"###....####.######\",
 \"###..######.######\",
-\"###########.######\" };")) t)
- '(helm-M-x-use-completion-styles nil)
- '(helm-apropos-fuzzy-match t)
- '(helm-buffers-fuzzy-matching t)
- '(helm-display-header-line nil)
- '(helm-grep-file-path-style (quote relative))
- '(helm-mode t)
- '(helm-recentf-fuzzy-match t)
- '(helm-split-window-inside-p t)
+\"###########.######\" };") t)
+ '(helm-ff-keep-cached-candidates nil)
+ '(helm-grep-file-path-style 'absolute)
  '(js-indent-level 2)
  '(org-agenda-files
-   (quote
-    ("~/src/puppetstack/meeting-notes.org" "~/org/work.org" "~/org/home.org")))
+   '("~/src/puppetstack/meeting-notes.org" "~/org/work.org" "~/org/home.org"))
  '(org-startup-truncated nil)
  '(org-use-speed-commands t)
  '(package-selected-packages
-   (quote
-    (rjsx-mode htmlize feature-mode indium terraform-mode tide typescript-mode flycheck-jest editorconfig handlebars-mode geben-helm-projectile auto-virtualenvwrapper magit-todos forge helm-rg moe-theme helm-swoop helm-smex helm-projectile helm js2-mode company-phpactor phpactor geben rust-mode counsel-gtags ws-butler ggtags ivy-phpunit ac-php string-inflection yasnippet loccur git-gutter company-php company markdown-mode nginx-mode jinja2-mode django-mode ivy-prescient flymake-python-pyflakes avy wgrep doom-themes flycheck syntax-subword ivy-hydra material-theme sublime-themes racket-mode yaml-mode puppet-mode dumb-jump zenburn-theme gruvbox-theme alect-themes organic-green-theme hamburg-theme counsel-projectile projectile ivy-mode exec-path-from-shell vagrant-tramp magit dart-mode paredit geiser slime counsel swiper ivy beacon use-package change-inner ido-grid-mode ido-vertical-mode ido-ubiquitous expand-region go-mode lua-mode gnu-apl-mode emmet-mode sql-indent php-mode web-mode abyss-theme rainbow-delimiters flx-ido flx smex)))
- '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#1d2021")))
+   '(php-mode projectile-ripgrep ripgrep consult-selectrum consult-flycheck flycheck-projectile marginalia consult selectrum-prescient selectrum prettier dockerfile-mode graphql-mode rjsx-mode htmlize feature-mode indium terraform-mode tide typescript-mode flycheck-jest editorconfig handlebars-mode geben-helm-projectile auto-virtualenvwrapper magit-todos forge helm-rg moe-theme helm-swoop helm-smex helm-projectile helm js2-mode company-phpactor phpactor geben rust-mode counsel-gtags ws-butler ggtags ivy-phpunit ac-php string-inflection yasnippet loccur git-gutter company-php company markdown-mode nginx-mode jinja2-mode django-mode ivy-prescient flymake-python-pyflakes avy wgrep doom-themes flycheck syntax-subword ivy-hydra material-theme sublime-themes racket-mode yaml-mode puppet-mode dumb-jump zenburn-theme gruvbox-theme alect-themes organic-green-theme hamburg-theme counsel-projectile projectile ivy-mode exec-path-from-shell vagrant-tramp magit dart-mode paredit geiser slime counsel swiper ivy beacon use-package change-inner ido-grid-mode ido-vertical-mode ido-ubiquitous expand-region go-mode lua-mode gnu-apl-mode emmet-mode sql-indent web-mode abyss-theme rainbow-delimiters flx-ido flx smex))
+ '(pdf-view-midnight-colors '("#fdf4c1" . "#1d2021"))
+ '(php-mode-template-compatibility nil)
  '(phpactor-executable "/Users/danr/.emacs.d/phpactor/phpactor.sh")
+ '(prettier-enabled-parsers
+   '(babel babel-flow css flow json json5 json-stringify less html markdown scss typescript vue yaml))
  '(projectile-enable-caching nil)
  '(projectile-mode t nil (projectile))
  '(tab-width 4)
+ '(tide-always-show-documentation t)
  '(vc-annotate-background "#222222")
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#fa5151")
+   '((20 . "#fa5151")
      (40 . "#ea3838")
      (60 . "#f8ffa0")
      (80 . "#e8e815")
@@ -522,7 +623,7 @@ static char *gnus-pointer[] = {
      (300 . "#94bff3")
      (320 . "#62b6ea")
      (340 . "#30a5f5")
-     (360 . "#e353b9"))))
+     (360 . "#e353b9")))
  '(vc-annotate-very-old-color "#e353b9")
  '(web-mode-enable-auto-indentation nil)
  '(web-mode-markup-indent-offset 4))
@@ -540,3 +641,4 @@ static char *gnus-pointer[] = {
 (put 'narrow-to-region 'disabled nil)
 ;;; init.el ends here
 (put 'magit-edit-line-commit 'disabled nil)
+(put 'list-timers 'disabled nil)
